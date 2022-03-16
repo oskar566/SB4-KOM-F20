aa5afa668045fdd75253326fe54fcd0cd52ef224
oskar566 <oschr20@student.sdu.dk>
2022-03-16 09:04:21 +0100

    Changes

---
+++ AsteroidsEntityFramework/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyPlugin.java
@@ -0,0 +1,47 @@
+package dk.sdu.mmmi.cbse.enemysystem;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.entityparts.LifePart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+
+public class EnemyPlugin implements IGamePluginService {
+
+    private Entity enemy;
+    public EnemyPlugin(){
+
+    }
+
+    @Override
+    public void start(GameData gameData, World world) {
+        enemy = createEnemyShip(gameData);
+        world.addEntity(enemy);
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+        world.removeEntity(enemy);
+    }
+
+    private Entity createEnemyShip (GameData gameData){
+
+        Entity enemyShip = new Enemy();
+
+        float deacceleration = 10;
+        float acceleration = 200;
+        float maxSpeed = 300;
+        float rotationSpeed = 5;
+        float x = gameData.getDisplayWidth() / 3;
+        float y = gameData.getDisplayHeight() / 3;
+        float radians = 3.1415f / 2;
+        enemyShip.add(new LifePart(3, 4));
+        enemyShip.setRadius(4);
+        enemyShip.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed));
+        enemyShip.add(new PositionPart(x, y, radians));
+
+        return enemyShip;
+    }
+}
